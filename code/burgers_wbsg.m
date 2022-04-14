42a855b3bb6bf2e76d149ee9011f435c9add1675
abhijit-c <abhijit9331@gmail.com>
2022-04-14 17:21:43 -0400

    Some updates to the code, this is more annoying data structure wise than expected.

--- code/burgers_wbsg.m
+++ code/burgers_wbsg.m
@@ -1,4 +1,4 @@
-function [u_h] = iburgers_wbsg(x_range, u0_h, ul, E, T, Nc)
+function [u_h] = iburgers_wbsg(x_range, u0_h, ul, E, T, dx)
 % burgers_wbsg.m solves the Burgers equation 
 % \[
 %   u_t(x,t,z) + f_x(u(x,t,z)) = -b'(x,z)u(x,t,z), (*)
@@ -18,19 +18,19 @@ function [u_h] = iburgers_wbsg(x_range, u0_h, ul, E, T, Nc)
 % Parameter List:
 % x_range : [x_range(1),x_range(2)] denotes the spatial domain to solve (*)
 %           over.
-% u0_h : Anonymous function describing the initial condition of the model
-%        represented in the basis of the orthogonal polynomials, i.e. 
+% u0_h : M x N matrix where the jth column corresponds to the cell average of
+%        the initial condition over the jth spatial cell expanded out in the
+%        orthogonal polynomial basis corresponding to z, i.e.:
 %        \[
-%          u0(x) = \sum_{m=1}^M u0_h(m) \Phi_m(x)
+%          avg(u0(x_j)) \approx \sum_{m=1}^M u0_h(m,j) polyval(Phi{m},x_j)
 %        \]
-%        Must be defined over x_range.
 % ul : Anonymous function describing the (Dirichlet) boundary condition at
 %      x_range(1).
 % E : M x M x M matrix where $E(k,m,n) = Expectation[\Phi_k \Phi_m \Phi_n]$,
 %     where $(\Phi_m)_{m=1}^M$ is the orthogonal polynomial basis descibed
 %     earlier, and the expectation is with respect to the law z obeys.
 % T : Final time to solve (*) up to.
-% Nc : Number of cells to divide x_range into.
+% dx : Parameter controlling width of spatial discretization.
 % Returned Variables:
 % u_hat: Coefficients of u in its generalized polynomial chaos expansion in the
 %        provided set of orthogonal polynomials
@@ -38,22 +38,33 @@ function [u_h] = iburgers_wbsg(x_range, u0_h, ul, E, T, Nc)
 % Authors: Andrew Shedlock & Abhijit Chowdhary, @ North Carolina State.
 % Date  : 2022/04/14
   f = @(u) u^2 / 2;
+
+  a = x_range(1); b = x_range(2);
+  M = size(u0_h)(1);
+  u_h = u0_h;
+  curr_t = 0.0;
+  while curr_t < T
+    % TODO: everything?
+    curr_t = curr_t + dt;
+  end
 end
 
-function A = build_A(E, u_h)
+function Aj = build_Aj(E, u_hj)
   M = length(E);
+  Aj = zeros(M, M);
   for m=1:M
     for n=1:M
-      A(m,n) = u_h' * E[:,m,n];
+      Aj(m,n) = u_hj' * E(:,m,n);
     end
   end
 end
 
-function B = build_B(E, b_h)
+function Bj = build_Bj(E, b_hj)
   M = length(E);
+  Bj = zeros(M, M);
   for m=1:M
     for n=1:M
-      b(m,n) = b_h' * E[:,m,n];
+      Bj(m,n) = b_hj' * E(:,m,n);
     end
   end
 end
