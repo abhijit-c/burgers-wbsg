4f2a45275a8789f9bbdf88332fceb8c1deed65b8
Andrew Shedlock <shedlock@10.191.248.93>
2022-04-22 21:18:41 -0400

    Figures for Example 1, currently working on the error analysis code for Example 1

--- code/Example1.m
+++ code/Example1.m
@@ -1,6 +1,6 @@
 clear; clc; clf;
 %% Setting up the conditions we will need to implement
-cell_count = 100; dx = 10/cell_count; x_range=[0,10]; T=10; M =5;
+cell_count = 100; dx = 10/cell_count; x_range=[0,10]; T=10; M =4;
 x=0.5*dx:dx:10-0.5*dx;
 
 
@@ -34,22 +34,59 @@ for k=1:M
     q=0.5*polyint(coeff);           % inner product
     coefficients(k)=diff(polyval(q,[-1 1]));
 end
-
-b_h = [zeros(M,1) coefficients*bottom(x)];  % We need to add a column for 
+b = bottom(x);
+b_h = [zeros(M,1) coefficients*b];  % We need to add a column for 
                                             % the left boundary condition 
                                             % which happens to be just zeros
 
 
+steady_mean = 2-2*b;
+steady_sd   = abs(b)/sqrt(3);
                                             
-%% Implementation
-u_wb = burgers_wbsg(u0_h, b_h, E, T, dx);       % WB      eq 3.14
-u_non = burgers_non_wbsg(u0_h, b_h, E, T, dx);  % Non WB  eq 3.21
+%% Well Balanced Scheme
 
-plot(x,u_wb(1,2:end),x,u_non(1,2:end));
+u_wb = burgers_wbsg(u0_h, b_h, E, T, dx);       % WB      eq 3.14
+mean_wb = u_wb(1,2:end); 
+sd_wb = u_wb.^2; sd_wb = sd_wb(2:end,2:end); sd_wb=sqrt(sum(sd_wb));
+
+figure(1)
+plot(x,steady_mean,'k--','LineWidth', 1.5); hold on
+plot(x,mean_wb,'o','Color',[0.6350 0.0780 0.1840],'LineWidth', 1.5);
+ylim([1.5,4.5])
+legend({'Steady State','Stochastic WB'},"Location","NorthEast",'FontSize',16);
+ax = gca; ax.FontSize = 18; 
+
+figure(2)
+plot(x,steady_sd,'k--','LineWidth', 1.5); hold on
+plot(x,sd_wb,'o','Color',[0.6350 0.0780 0.1840],'LineWidth', 1.5);
+ylim([0,0.7])
+legend({'Steady State','Stochastic WB'},"Location","NorthEast",'FontSize',16);
+ax = gca; ax.FontSize = 18; 
+
+%% Non-Well Balanced Scheme
+
+u_non = burgers_non_wbsg(u0_h, b_h, E, T, dx);  % Non WB  eq 3.21
+mean_non = u_non(1,2:end); sd_non = u_non.^2; 
+sd_non = sd_non(2:end,2:end); sd_non=sqrt(sum(sd_non));
+
+figure(3)
+plot(x,steady_mean,'k--','LineWidth', 1.5); hold on
+plot(x,mean_non,'^','Color',[0.9290 0.6940 0.1250],'LineWidth', 1.5);
+ylim([1.5,4.5])
+legend({'Steady State','gPC Non-WB'},"Location","NorthEast",'FontSize',16);
+ax = gca; ax.FontSize = 18; 
+
+figure(4)
+plot(x,steady_sd,'k--','LineWidth', 1.5); hold on
+plot(x,sd_non,'^','Color',[0.9290 0.6940 0.1250],'LineWidth', 1.5);
+ylim([0,0.7])
+legend({'Steady State','gPC Non-WB'},"Location","NorthEast",'FontSize',16);
+ax = gca; ax.FontSize = 18; 
+
 
 %% Functions Used
 
-%Has to be called one time for each instance of random variable
+% Has to be called one time for each instance of random variable
 function b = bottom(x)
     b=zeros(size(x)); N=length(x);
     for j=1:N
